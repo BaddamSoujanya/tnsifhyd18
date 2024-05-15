61254609a28d005d82c51c6a73928c08f9a42268
BaddamSoujanya <168448491+BaddamSoujanya@users.noreply.github.com>
2024-05-15 14:23:05 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,13 @@
+package Project1;
+
+public class employee {
+	int employeeid=1;
+	static employee soujanya;
+
+	public static void main(String[] args) {
+		 int employeesalary=30000 ;
+		System.out.println(employeesalary);
+		
+	}
+
+}
---
+++ program1.java
@@ -0,0 +1,10 @@
+package Project1;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
